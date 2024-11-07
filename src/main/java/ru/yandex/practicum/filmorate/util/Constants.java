0c8af91d6b850b648e08daab1e402ba8106079de
Konstantin Shushkevich <shusha113@yandex.ru>
2024-11-07 14:43:25 +0300

    Sprint 10 hw

--- src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
@@ -3,5 +3,5 @@ package ru.yandex.practicum.filmorate.util;
 import java.time.LocalDate;
 
 public final class Constants {
-    public final static LocalDate START_OF_CINEMA_EPOCH = LocalDate.of(1895, 12, 27);
+    public static final LocalDate START_OF_CINEMA_EPOCH = LocalDate.of(1895, 12, 27);
 }
